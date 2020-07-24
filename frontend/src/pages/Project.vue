c989e08a1e1c8c68d226399274e29f8634e06b5d
Fedya Potapchenko <potapchenkof@gmail.com>
2020-07-24 22:36:07 +0300

    FIXED BUGS

--- frontend/src/pages/Project.vue
+++ frontend/src/pages/Project.vue
@@ -1,9 +1,23 @@
 <template>
     <v-container>
-        <div align="center">Project
-            <h1>{{project.name}}</h1>
-        </div>
-        <div class="mt-10">
+        <v-layout class="justify-space-between row">
+            <router-link :to="`/main`">
+                <v-btn class="py-8 px-0" rounded text>
+                    <v-icon class="" color="grey" size="40px">
+                        arrow_back_ios
+                    </v-icon>
+                </v-btn>
+            </router-link>
+
+            <v-flex class="d-flex justify-center">
+                <div class="text-lg-center">
+                    <v-card-title primary-title class="layout justify-center">Project</v-card-title>
+                    <h1>{{project.name}}</h1>
+                </div>
+            </v-flex>
+        </v-layout>
+
+        <div class="mt-16">
             <b>Description:</b>
             {{project.description}}
             <div class="mt-2">
@@ -11,9 +25,9 @@
             </div>
             <h3 class="mt-10">Tasks:</h3>
             <div v-if="project.tasks && project.tasks.length">
-            <task-list
-                    :tasks="project.tasks"
-            ></task-list>
+                <task-list
+                        :tasks="project.tasks"
+                ></task-list>
             </div>
             <div v-else>Empty list.</div>
         </div>
@@ -55,4 +69,7 @@
 </script>
 
 <style scoped>
+    a {
+        text-decoration: none;
+    }
 </style>
